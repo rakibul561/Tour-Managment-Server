8af0c8884d676638c48215295239fa825f3c7a03
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-07-08 10:58:19 -0700

    server error fixing

--- src/server.ts
+++ src/server.ts
@@ -27,3 +27,66 @@ const startServer = async () =>{
 startServer();
 
 
+   process.on("SIGTERM", () =>{
+    console.log("SIGTERM signal recieved........ Server sutting Down");
+
+    if(server){
+        server.close(() =>{
+        process.exit(1)
+
+        });
+    }
+    process.exit();
+    
+})
+
+   process.on("SIGINT", () =>{
+    console.log("SIGTERM signal recieved........ Server sutting Down");
+
+    if(server){
+        server.close(() =>{
+        process.exit(1)
+
+        });
+    }
+    process.exit();
+    
+})
+
+   
+
+
+ 
+process.on("unhandledRejection", (err) =>{
+    console.log("unhandale Rejection detected........ Server sutting Down", err);
+
+    if(server){
+        server.close(() =>{
+        process.exit(1)
+
+        });
+    }
+    process.exit();
+    
+})
+process.on("uncaughtException", (err) =>{
+    console.log("uncaught Exception detected........ Server sutting Down", err);
+
+    if(server){
+        server.close(() =>{
+        process.exit(1)
+
+        });
+    }
+    process.exit();
+    
+})
+ 
+// unhandler rejection Error
+// Promise.reject(new Error (" I forgot to catch this promise "))
+
+
+// uncaught Exception Error 
+// throw new Error("I forgot to handle this localhost!")
+
+

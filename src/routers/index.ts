a0e0bc0f6f2957b1a898fb35a9775cf339d6bc1e
Rakibul hasan <rakibulhasan3929@gmail.com>
2025-07-25 04:13:26 -0700

    tour complete

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express"
 import { UserRoutes } from "../app/modules/user/user.route"
 import { AuthRoutes } from "../app/modules/auth/auth.route"
+import { DivisionRoutes } from "../app/modules/division/division.route"
+import { TourRoutes } from "../app/modules/tour/tour.route"
 
 export const router = Router()
 
@@ -13,10 +15,26 @@ const moduleRoutes = [
         path: "/auth",
         route: AuthRoutes
     },
-  
+    {
+        path: "/division",
+        route: DivisionRoutes
+    },
+    {
+        path: "/tour",
+        route: TourRoutes
+    }
+    // {
+    //     path: "/tour",
+    //     route: TourRoutes
+    // },
 ]
 
 moduleRoutes.forEach((route) => {
     router.use(route.path, route.route)
 })
 
+// router.use("/user", UserRoutes)
+// router.use("/tour", TourRoutes)
+// router.use("/division", DivisionRoutes)
+// router.use("/booking", BookingRoutes)
+// router.use("/user", UserRoutes)
